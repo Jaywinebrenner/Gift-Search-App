160f71843dd954d5d6f01432a24a6c7a40b7bd12
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-26 14:53:44 -0700

    switch from fetch call to axios call

--- src/App.js
+++ src/App.js
@@ -2,22 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
+import axios from 'axios'
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
-  } 
+    this.state = {
+      gifs: []
+    }
+  }
 
-  render() { 
+
+// THE AXIOS METHOD TO MAKE AN API CALL - INSTALL AXIOS, IMPORT AXIOS
+  componentDidMount() {
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=HsztyzWl2xSDhQj6OiDJT1vvFPg0Nqcw')
+    .then( response => {
+      this.setState({
+        gifs: response.data.data
+      })
+    })
+    .catch( error => {
+
+      console.log("error fetaching and parsing data", error);
+    })
+    .finally(function () {
+
+    });
+  }
+
+
+  // THE FETCH METHOD TO MAKE API CALL
+  //   componentDidMount() {
+  //     fetch('http://api.giphy.com/v1/gifs/trending?api_key=HsztyzWl2xSDhQj6OiDJT1vvFPg0Nqcw')
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       this.setState({ gifs: responseData.data})
+  //     })
+  //     .catch(error => {
+  //       console.log("Error fetching and parsing data", error);
+  //     });
+  //   }
+
+  render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
